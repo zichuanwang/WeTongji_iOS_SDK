18ada24141eeca88952043f2e8ad5dfc5feec27d
bluebitch <wzc345@gmail.com>
2012-10-22 23:47:54 +0800

    Initial Commit

---
+++ WeTongjiSDK/WeTongjiSDK.h
@@ -0,0 +1,13 @@
+//
+//  WeTongjiSDK.h
+//  WeTongjiSDK
+//
+//  Created by 王 紫川 on 12-10-22.
+//  Copyright (c) 2012年 WeTongji. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WeTongjiSDK : NSObject
+
+@end
---
+++ WeTongjiSDK/WeTongjiSDK.m
@@ -0,0 +1,13 @@
+//
+//  WeTongjiSDK.m
+//  WeTongjiSDK
+//
+//  Created by 王 紫川 on 12-10-22.
+//  Copyright (c) 2012年 WeTongji. All rights reserved.
+//
+
+#import "WeTongjiSDK.h"
+
+@implementation WeTongjiSDK
+
+@end
