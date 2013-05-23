455e6711031487ed7f7da22c6ced081cbf2a271b
bluebitch <wzc345@gmail.com>
2013-05-23 10:59:38 +0800

    切换测试服务器时刷新client.

--- WeTongjiSDK/NSUserDefaults+WTSDKAddition.m
+++ WeTongjiSDK/NSUserDefaults+WTSDKAddition.m
@@ -7,6 +7,7 @@
 //
 
 #import "NSUserDefaults+WTSDKAddition.h"
+#import "WTClient.h"
 
 #define kCurrentUserID              @"kCurrentUserID"
 #define kCurrentUserSession         @"kCurrentUserSession"
@@ -41,6 +42,7 @@
 + (void)setUseTestServer:(BOOL)useTestServer {
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     [defaults setBool:useTestServer forKey:kUseTestServer];
+    [WTClient refreshSharedClient];
     [defaults synchronize];
 }
 
